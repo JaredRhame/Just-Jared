613f321e742e3aae5f7d260a02dd81aca3e089e4
JaredRhame <[jared.rhame@gmail.com]>
2019-01-14 01:10:50 -0500

    Refactored the project info pages into one component. Each projects info is passed through state to the ProjInfo comp.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,32 +13,74 @@ class Projects extends Component {
   }
 
   render() {
+    const projOne = {
+      name: "Giphy Clone",
+      picUrl: "../imgs/giphy-clone.JPG",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
+    };
+    const projTwo = {
+      name: "Snapple Facts",
+      picUrl: "/imgs/giphy-clone.JPG",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
+    };
+    const projThree = {
+      name: "Something Awesome",
+      picUrl: "./imgs/giphy-clone.JPG",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
+    };
+    const projFour = {
+      name: "Eventually Something Amazing",
+      picUrl: "./imgs/giphy-clone.JPG",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
+    };
+
     return (
       <div className="projects z-depth-5">
         <h2 className="headings">Projects</h2>
         <div id="proj-grid">
-          <Link to="/Tile1">
+          <Link
+            to={{
+              pathname: "/proj-info",
+              state: {
+                project: projOne
+              }
+            }}
+          >
             <div id="proj-1" className="project-tile" />
           </Link>
-
-          <Link to="/Tile2">
+          <Link
+            to={{
+              pathname: "/proj-info",
+              state: {
+                project: projTwo
+              }
+            }}
+          >
             <div id="proj-2" className="project-tile" />
           </Link>
-
-          <Link to="/Tile3">
+          <Link
+            to={{
+              pathname: "/proj-info",
+              state: {
+                project: projThree
+              }
+            }}
+          >
             <div id="proj-3" className="project-tile" />
           </Link>
-
-          <Link to="/Tile4">
+          <Link
+            to={{
+              pathname: "/proj-info",
+              state: {
+                project: projFour
+              }
+            }}
+          >
             <div id="proj-4" className="project-tile" />
-          </Link>
-
-          <Link to="/Tile5">
-            <div id="proj-5" className="project-tile" />
-          </Link>
-
-          <Link to="/Tile6">
-            <div id="proj-6" className="project-tile" />
           </Link>
         </div>
       </div>
@@ -47,3 +89,10 @@ class Projects extends Component {
 }
 
 export default Projects;
+
+const projOne = {
+  name: "Giphy Clone",
+  picUrl: "./imgs/giphy-clone.JPG",
+  content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
+};
