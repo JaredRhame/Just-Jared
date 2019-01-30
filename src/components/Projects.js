56133c96a45d1254cfec8ddc7004673d528ba7f2
JaredRhame <[jared.rhame@gmail.com]>
2019-01-30 01:40:33 -0500

    Added an about me paragraph and description for the Giphy Clone project.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,28 +18,24 @@ class Projects extends Component {
     const projOne = {
       id: "proj-one",
       name: "Giphy Clone",
-      picUrl: "../imgs/giphy-clone.JPG",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
+        "This project allows the user to enter their desired search term to populate the DOM with related gifs. This was achieved by using the Fetch API in combination with the Giphy API. I learned how to handle the returned promise in order to obtain the correct JSON data that needed to be displayed."
     };
     const projTwo = {
       id: "proj-two",
       name: "Snapple Facts",
-      picUrl: "/imgs/under-construction.jpg",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
     };
     const projThree = {
       id: "proj-three",
       name: "Something Awesome",
-      picUrl: "./imgs/giphy-clone.JPG",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
     };
     const projFour = {
       id: "proj-four",
       name: "Eventually Something Amazing",
-      picUrl: "./imgs/giphy-clone.JPG",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue massa risus, vitae eleifend eros ullamcorper ornare. Nulla facilisi. Maecenas et nunc a massa lacinia tincidunt. Mauris tincidunt vestibulum lacus, eget posuere felis dictum in. Suspendisse ac semper odio. Duis lorem erat, imperdiet quis convallis eget, gravida vel massa. Vivamus condimentum non lacus non vehicula. Fusce eget vulputate erat, sit amet varius eros. Curabitur pretium magna nec purus pretium, ut vestibulum est mattis. Morbi risus enim, consectetur et consequat nec, ultrices sit amet sapien. Sed a elit quis nisi suscipit pharetra quis at velit. Duis commodo in erat at tincidunt. Nulla scelerisque massa et arcu hendrerit egestas. Suspendisse potenti. Maecenas eleifend libero erat, sed condimentum velit viverra in. Vivamus felis tellus, elementum non dolor porta, commodo ultricies purus. Praesent eget tellus turpis. Etiam nisi metus, elementum sit amet libero eu, porta maximus elit."
     };
